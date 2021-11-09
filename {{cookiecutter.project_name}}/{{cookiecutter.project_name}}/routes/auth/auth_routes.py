06a995264ced42a59565f1f703bab7bfed8e7cc1
nelsoHolic <nelson.holic@gmail.com>
2021-11-09 08:57:02 -0500

    adding fixes for auth code

--- {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/routes/auth/auth_routes.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/routes/auth/auth_routes.py
@@ -19,7 +19,7 @@ async def create_user_endpoint(user_data: UserPayload, db_session: Session = Dep
     if get_user_by_username(user_data.username, db_session):
         raise HTTPException(status_code=403, detail="That username is already in used")
 
-    create_user(user_data)
+    create_user(user_data, db_session)
 
     return "success"
 
